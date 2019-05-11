786f83624037262651fb62f14a08bad7202da749
DESKTOP-FI1D7HI\pontura <pontura@gmail.com>
2019-05-10 18:25:06 -0700

    summary

--- ceamse/Assets/Summary.cs
+++ ceamse/Assets/Summary.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Summary : MonoBehaviour
 {
+    public SceneObject[] masks;
 
     public Text field;
     void Start()
@@ -14,10 +15,33 @@ public class Summary : MonoBehaviour
             field.text = "No pudiste reciclar ningún objeto. ¡Más suerte la próxima!";
         else
             field.text = "¡Bravo! Reciclaste " + total  +  " objetos.";
+
+        SetMasks();
     }
     public void Restart()
     {
         Events.ResetGame();
         UnityEngine.SceneManagement.SceneManager.LoadScene("Splash");
+    }
+    void SetMasks()
+    {
+        foreach(Data.ResultsItem ri in Data.Instance.results)
+        {
+            SceneObject so = GetMask(ri.type);
+            so.GetComponentInChildren<SummaryNum>().Init(ri.correct_qty);
+            if (ri.correct_qty == 0)
+                so.gameObject.SetActive(true);
+            else
+                so.gameObject.SetActive(false);
+        }
+    }
+    SceneObject GetMask(SceneObject.types type)
+    {
+        foreach(SceneObject so in masks)
+        {
+            if(so.type == type)
+            return so;
+        }
+        return null;
     }
 }
---
+++ ceamse/Assets/SummaryNum.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SummaryNum : MonoBehaviour
+{
+    public Text field;
+
+   public void Init(int total)
+    {
+        if(total == 0)
+        {
+            this.gameObject.SetActive(false);
+            return;
+        }
+        else
+        {
+            this.gameObject.SetActive(true);
+            field.text = total.ToString();
+        }
+    }
+}
